18e8d665548568a7f4e586602a1cde118a4dfd97
Chan Pham <iamchanpham@gmail.com>
2024-09-30 12:50:21 +0000

    escape room template

---
+++ index.js
@@ -0,0 +1,79 @@
+// 1st Floor
+let gymatoriumLock = true;
+
+// Commit and show us to unlock the next floor's instructions
+// ----------------------------
+// 2nd Floor
+// Do not modify these arrays directly
+let weworkItems = ["loaner laptop", "whiteboard", "conference room", "couch", "printer", "shared table", "Wi-Fi router", "plants", "office chair", "power outlets"];
+let computerLabItems = ["desktop computers", "monitors", "broken laptop", "mice", "Wi-Fi router", "printer", "power strips", "headphones", "USB drives", "chairs", "desks"];
+let mprItems = ["mirrors", "speakers", "chairs", "portable stereo", "projector", "chairs", "posters"];
+
+
+// Commit and show us to unlock the next floor's instructions
+// ----------------------------
+// 3rd Floor
+// Do not modify these objects directly
+let scoreValues = {
+    "S": "Self-awareness",
+    "C": "Challenge",
+    "O": "Organization",
+    "R": "Resilience"
+}
+
+let afterSchoolClubs = {
+    athletic: [
+        {
+            club: "Soccer Club",
+            location: "St. Mary's Park"
+        },
+        {
+            club: "Flag Football",
+            location: "St. Mary's Park"
+        },
+        {
+            club: "Volleyball",
+            location: "Gymnatorium"
+        }
+    ],
+    social: [
+        {
+            club: "Gaming Club",
+            location: 305
+        },
+        {
+            club: "Art Club",
+            location: 417
+        },
+        {
+            club: "Gardening Club",
+            location: 311
+        }     
+    ]
+}
+
+
+// Commit and show us to unlock the next floor's instructions
+// ----------------------------
+// 4th Floor
+// Uncomment the code below to begin this task
+/*
+let takeCareOfPet = function(snacks, pets) => {
+    if (Snacks[2] === "hotdogs" && pets > 10) {
+        return "Raccoon has been appeased"
+    } else {
+        return "The raccoon bit you and you died of rabies."
+    
+}
+*/
+
+
+// Commit and show us to unlock the next floor's instructions
+// ----------------------------
+// 5th Floor
+
+
+
+
+
+// Commit and show us to unlock the next floor's instructions
